36327f68bfff7ca1992ca6decbd22efdecad4a1e
Brando Endona <cbrandoendona@gmail.com>
2025-01-20 16:28:24 +0800

    refactor page.js

--- src/app/page.js
+++ src/app/page.js
@@ -14,15 +14,22 @@ export default function Home() {
   useEffect(() => {
     if (echo) {
       console.log(echo)
-      echo.private(`cases.${user.iop_code}`).listen("SelfCaseCreated", (subscriptionData) => {
-        console.log(subscriptionData)
-      })
+      echo
+        .private(`cases.${user.iop_code}`)
+        .listen("SelfCaseCreated", (subscriptionData) => {
+          toast(`a new case has been created for ${user?.iop_code}`)
+          console.log(subscriptionData)
+        })
     }
   }, [user])
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Navigation />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1 className="text-3xl">
+          Tenant: <span className="text-indigo-500">{user?.iop_code}</span>
+        </h1>
+      </main>
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
